9c1a91a3bb6bf0abdf6631bc6567b42adb3a7443
YannDubs <yanndubois96@gmail.com>
2021-10-31 20:18:07 -0400

    rm all previous files.

--- issl/loss_sample_eff.py
+++
@@ -1,6 +0,0 @@
-def get_loss_sample_eff():
-    pass
-
-class Generative
-
-class COntrastive
--- issl/losses/sample_eff/information.py
+++
@@ -1,2 +0,0 @@
-"""Regularizers for I[Z,X] and thus improve downstream sample efficiency."""
-from __future__ import annotations
