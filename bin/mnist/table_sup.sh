f8627569754d03a7b7e7c9b148da3dcb805789d2
YannDubs <yanndubois96@gmail.com>
2021-11-02 06:23:40 -0400

    [BIN] table sup

---
+++ bin/mnist/table_sup.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+experiment=$prfx"table_sup"
+notes="
+**Goal**: run the dim part of the MNIST table.
+"
+
+# parses special mode for running the script
+source `dirname $0`/../utils.sh
+
+# define all the arguments modified or added to `conf`. If they are added use `+`
+kwargs="
+experiment=$experiment
++logger.wandb_kwargs.project=mnist
+trainer.max_epochs=50
+checkpoint@checkpoint_repr=bestTrainLoss
+architecture@encoder=resnet18
+architecture@online_evaluator=linear
+data@data_repr=mnist
+data_pred.all_data=[data_repr_agg,data_repr_30_test,data_repr_100,data_repr_1000,data_repr_10000]
+predictor=sk_logistic
+data_repr.kwargs.val_size=2
++data_pred.kwargs.val_size=2
++trainer.num_sanity_val_steps=0
++trainer.limit_val_batches=0
+representor=none
+predictor=pytorch
+architecture@predictor=resnet18
+timeout=$time
+$add_kwargs
+"
+
+
+# every arguments that you are sweeping over
+kwargs_multi="
+representor=none
+seed=1,2,3
+"
+
+# difference for gen: linear resnet / augmentations / larger dim
+
+
+if [ "$is_plot_only" = false ] ; then
+  for kwargs_dep in  ""
+  do
+
+    python "$main" +hydra.job.env_set.WANDB_NOTES="\"${notes}\"" $kwargs $kwargs_multi $kwargs_dep -m &
+
+    sleep 3
+
+  done
+fi
