f3992a5630508537f25c9e3901fea1966bb6b033
Yann Dubois <yanndubois96@gmail.com>
2022-01-20 11:00:30 -0800

    [HOPT] whitening tin

--- bin/tinyimagenet/whitening_tin.sh
+++ bin/tinyimagenet/whitening_tin.sh
@@ -8,7 +8,7 @@ notes="
 # parses special mode for running the script
 source `dirname $0`/../utils.sh
 
-time=4320
+time=5760
 
 # define all the arguments modified or added to `conf`. If they are added use `+`
 kwargs="
@@ -20,7 +20,6 @@ downstream_task.all_tasks=[sklogistic_datarepr,sklogistic_datarepr001test,sklogi
 ++data_pred.kwargs.val_size=2
 +trainer.num_sanity_val_steps=0
 representor=std_cntr
-decodability.kwargs.temperature=0.5
 data_repr.kwargs.batch_size=512
 scheduler_issl.kwargs.base.is_warmup_lr=True
 decodability.kwargs.projector_kwargs.hid_dim=1024
@@ -31,6 +30,7 @@ checkpoint@checkpoint_repr=bestTrainLoss
 +trainer.limit_val_batches=0
 ++data_repr.kwargs.val_size=2
 optimizer@optimizer_issl=AdamW
+scheduler@scheduler_issl=warm_unifmultistep
 timeout=$time
 "
 
@@ -43,17 +43,20 @@ hydra/sweeper/sampler=random
 hypopt=optuna
 monitor_direction=[maximize]
 monitor_return=[pred/tinyimagenet/accuracy_score]
-hydra.sweeper.n_trials=5
-hydra.sweeper.n_jobs=5
-hydra.sweeper.study_name=v2
+hydra.sweeper.n_trials=10
+hydra.sweeper.n_jobs=10
+hydra.sweeper.study_name=v1
 decodability.kwargs.projector_kwargs.out_shape=64
 optimizer_issl.kwargs.lr=tag(log,interval(1e-3,5e-3))
 optimizer_issl.kwargs.weight_decay=tag(log,interval(5e-7,5e-6))
-scheduler@scheduler_issl=warm_unifmultistep100,slowwarm_unifmultistep25,warm_unifmultistep125
+scheduler_issl.kwargs.UniformMultiStepLR.decay_per_step=shuffle(range(3,8))
+scheduler_issl.kwargs.base.warmup_epochs=interval(0,0.3)
 seed=1,2,3,4,5,6,7,8,9
-trainer.max_epochs=1000
+decodability.kwargs.temperature=0.3,0.5
+trainer.max_epochs=300
 "
 # to replicate should add 1000 epochs and warm_unifmultistep125
+# they used decodability.kwargs.temperature=0.5 but given that improves a lot when decreased should also be tuned
 
 
 # difference for gen: linear resnet / augmentations / larger dim
