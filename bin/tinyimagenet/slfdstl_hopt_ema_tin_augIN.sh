954889e5b7a692b5b3df7be4b0f7120c239c01ae
Yann Dubois <yanndubois96@gmail.com>
2022-02-07 17:22:30 -0800

    [HOPT] make slfdstl ema good.

--- bin/tinyimagenet/slfdstl_hopt_ema_tin_augIN.sh
+++ bin/tinyimagenet/slfdstl_hopt_ema_tin_augIN.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-experiment="slfdst_hopt_tin"
+experiment="slfdst_hopt_ema_tin"
 notes="
-**Goal**: hyperparameter tuning for selfdistillation on tinyimagenet.
+**Goal**: make sefldistillation work with ema.
 "
 
 # parses special mode for running the script
@@ -17,24 +17,26 @@ time=10080
 kwargs="
 experiment=$experiment
 $base_kwargs_tin
-representor=slfdstl
+representor=slfdstl_augIN
 data_repr.kwargs.batch_size=256
-representor.loss.beta=1e-6
-decodability.kwargs.beta_pM_unif=1.7
-decodability.kwargs.ema_weight_prior=null
+decodability.kwargs.ema_weight_prior=0.5
+scheduler_issl.kwargs.UniformMultiStepLR.k_steps=5
+scheduler_issl.kwargs.UniformMultiStepLR.decay_per_step=3
 timeout=$time
 "
 
 kwargs_multi="
 seed=3
-trainer.max_epochs=1000
-decodability.kwargs.out_dim=7000
-scheduler_issl.kwargs.UniformMultiStepLR.k_steps=5
-scheduler_issl.kwargs.UniformMultiStepLR.decay_per_step=3
+trainer.max_epochs=500
+decodability.kwargs.out_dim=500,7000
+representor.loss.beta=1e-6
+decodability.kwargs.beta_pM_unif=1.7
+regularizer=huber
+optimizer_issl.kwargs.weight_decay=1e-6
 "
 
 if [ "$is_plot_only" = false ] ; then
-  for kwargs_dep in  "" "decodability.kwargs.out_dim=10000,15000"  "regularizer=cosine" "regularizer=huber representor.loss.beta=1e-6,5e-6"
+  for kwargs_dep in  ""
   do
 
     python "$main" +hydra.job.env_set.WANDB_NOTES="\"${notes}\"" $kwargs $kwargs_multi $kwargs_dep $add_kwargs  -m &
