1b4091616a69f9a64b86c5861ab8881be0c63b0b
Yann Dubois <yanndubois96@gmail.com>
2022-04-19 05:13:42 -0700

    [ENH] try except rank

--- issl/callbacks.py
+++ issl/callbacks.py
@@ -138,8 +138,12 @@ class EffectiveDim(PlottingCallback):
 
     def on_train_epoch_end(self, trainer, pl_module, *args, **kwargs):
         corr_coef = self.train_corr_coef / self.n_train_steps
-        rank = torch.linalg.matrix_rank(corr_coef, atol=1e-4, rtol=0.01, hermitian=True).float()
-        pl_module.log(f"train/{pl_module.stage}/{pl_module.hparams.task}/rank", rank, on_epoch=True)
+
+        try:
+            rank = torch.linalg.matrix_rank(corr_coef, atol=1e-4, rtol=0.01, hermitian=True).float()
+            pl_module.log(f"train/{pl_module.stage}/{pl_module.hparams.task}/rank", rank, on_epoch=True)
+        except:
+            logger.warning("could not compute rank")
 
         super().on_train_epoch_end(trainer, pl_module, *args, **kwargs)
 
@@ -148,14 +152,21 @@ class EffectiveDim(PlottingCallback):
 
     def on_validation_epoch_end(self, trainer, pl_module, *args, **kwargs):
         corr_coef = self.val_corr_coef / self.n_val_steps
-        rank = torch.linalg.matrix_rank(corr_coef, atol=1e-4, rtol=0.01, hermitian=True).float()
-        pl_module.log(f"train/{pl_module.stage}/{pl_module.hparams.task}/rank", rank, on_epoch=True)
+
+        try:
+            rank = torch.linalg.matrix_rank(corr_coef, atol=1e-4, rtol=0.01, hermitian=True).float()
+            pl_module.log(f"train/{pl_module.stage}/{pl_module.hparams.task}/rank", rank, on_epoch=True)
+        except:
+            logger.warning("could not compute rank")
+
         self.n_val_steps = 0
         self.val_corr_coef = 0
 
     def yield_figs_kwargs(self, trainer: pl.Trainer, pl_module: pl.LightningModule):
         with torch.no_grad():
             corr_coef = self.train_corr_coef / self.n_train_steps
+
+
             eigenv = torch.linalg.eigvalsh(corr_coef)
             eigenv = eigenv.abs().log().sort(descending=True)[0]
 
