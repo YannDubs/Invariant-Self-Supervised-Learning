e3119c0a0cad908eef3c6bf9bf0b1d7bddc8ae86
Yann Dubois <yanndubois96@gmail.com>
2022-04-26 03:01:33 -0700

    [HOPT] last hparam tuning for cntr resnet.

--- bin/tinyimagenet/resnet50_cntr_tin.sh
+++ bin/tinyimagenet/resnet50_cntr_tin.sh
@@ -13,46 +13,19 @@ time=10080
 
 kwargs="
 experiment=$experiment
-++logger.wandb_kwargs.project=tinyimagenet
-architecture@encoder=resnet18
-architecture@online_evaluator=linear
-encoder.kwargs.arch_kwargs.is_no_linear=True
-data@data_repr=tinyimagenet
-data_repr.kwargs.is_force_all_train=True
-data_repr.kwargs.is_val_on_test=True
-checkpoint@checkpoint_repr=last
-optimizer@optimizer_issl=Adam
-scheduler@scheduler_issl=cosine
-optimizer_issl.kwargs.weight_decay=1e-6
-optimizer_issl.kwargs.lr=2e-3
+$base_kwargs_tin
 representor=cntr
-data_repr.kwargs.batch_size=512
-architecture@encoder=resnet50
-downstream_task.all_tasks=[torchlogisticw1e-5_datarepr,torchlogisticw1e-4_datarepr,torchlogistic_datarepr]
-timeout=$time
 seed=1
-encoder.rm_out_chan_aug=False
-encoder.z_shape=2048
-update_trainer_repr.max_epochs=200
+data_repr.kwargs.batch_size=512
+downstream_task.all_tasks=[torchlogistic_datarepr,torchlogisticw1e-5_datarepr,torchlogisticw1e-4_datarepr]
+timeout=$time
 "
 
 kwargs_multi="
-hydra/sweeper=optuna
-hydra/sweeper/sampler=random
-hypopt=optuna
-monitor_direction=[maximize]
-monitor_return=[pred/torchlogistic_datarepr/acc]
-hydra.sweeper.study_name=new1
-hydra.sweeper.n_trials=15
-hydra.sweeper.n_jobs=15
-decodability.kwargs.is_self_contrastive=True,False
-decodability.kwargs.predictor_kwargs.out_shape=128,512
-regularizer=none,etf,huber
-representor.loss.beta=1e-1,1e-2
 "
 
 if [ "$is_plot_only" = false ] ; then
-  for kwargs_dep in  ""
+  for kwargs_dep in  "regularizer=etf representor.loss.beta=1e-1,1e-3" "regularizer=none,etf,huber,effdim" "decodability.kwargs.predictor_kwargs.out_shape=128,512"
   do
 
     python "$main" +hydra.job.env_set.WANDB_NOTES="\"${notes}\"" $kwargs $kwargs_multi $kwargs_dep $add_kwargs  -m  >> logs/"$experiment".log 2>&1 &
