bd2f9568b1fd4800e0d72395b696bcd83cdf344d
Yann Dubois <yanndubois96@gmail.com>
2022-08-13 20:14:12 -0700

    dd noselfcntr

--- bin/tinyimagenet/table_cntr.sh
+++ bin/tinyimagenet/table_cntr.sh
@@ -32,6 +32,10 @@ cell_ours="
 representor=cntr
 "
 
+cell_ours_noselfcntr="
+representor=cntr
+decodability.kwargs.is_self_contrastive=False
+"
 
 cell_dim="
 encoder.z_shape=2048
@@ -67,7 +71,7 @@ update_trainer_repr.max_epochs=1000
 "
 
 if [ "$is_plot_only" = false ] ; then
-  for kwargs_dep in "$cell_aug_noblured" "$cell_aug_onlyblured"  #"$cell_baseline" "$cell_ours"  "$cell_dim"  "$cell_aug" "$cell_epoch"
+  for kwargs_dep in "$cell_ours_noselfcntr" # "$cell_aug_noblured" "$cell_aug_onlyblured"  #"$cell_baseline" "$cell_ours"  "$cell_dim"  "$cell_aug" "$cell_epoch"
   do
     python "$main" +hydra.job.env_set.WANDB_NOTES="\"${notes}\"" $kwargs $kwargs_multi $kwargs_dep $add_kwargs -m >> logs/"$experiment".log 2>&1 &
 
