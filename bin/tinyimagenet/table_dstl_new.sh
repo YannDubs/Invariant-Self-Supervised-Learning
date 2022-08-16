6d59164e63b0937a8b792a5cd9cce2bed11b5da7
Yann Dubois <yanndubois96@gmail.com>
2022-08-15 23:21:20 -0700

    continue runs

--- bin/tinyimagenet/table_dstl_new.sh
+++ bin/tinyimagenet/table_dstl_new.sh
@@ -102,7 +102,7 @@ update_trainer_repr.max_epochs=1000
 "
 
 if [ "$is_plot_only" = false ] ; then
-  for kwargs_dep in "$cell_nocosine" # "$cell_bnpre" # "$cell_noJL" #"$cell_large_proj" # "$cell_epoch_coarse" #"$cell_ours" #  #"$cell_ours"   # "$cell_aug" "$cell_epoch" "$cell_ours" "$cell_dim"   #  "$cell_ours" "$cell_dim" "$cell_asymm_small" "$cell_asymm_large" "$cell_aug" "$cell_epoch" # cell_baseline
+  for kwargs_dep in "$cell_bnpre" # "$cell_bnpre" # "$cell_noJL" #"$cell_large_proj" # "$cell_epoch_coarse" #"$cell_ours" #  #"$cell_ours"   # "$cell_aug" "$cell_epoch" "$cell_ours" "$cell_dim"   #  "$cell_ours" "$cell_dim" "$cell_asymm_small" "$cell_asymm_large" "$cell_aug" "$cell_epoch" # cell_baseline
   do
 
     python "$main" +hydra.job.env_set.WANDB_NOTES="\"${notes}\"" $kwargs $kwargs_multi $kwargs_dep $add_kwargs -m >> logs/"$experiment".log 2>&1 &
